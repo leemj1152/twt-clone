cd923555ea7f286e21427964b8b59abc1bee3cc4
leemj1152 <lee3321998@naver.com>
2023-04-25 14:51:35 +0900

    중간 보류 //

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import { authService } from "../fbase";
 import AppRouter from "./Router"
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {  
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => console.log(user))
+  }, [])
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn}/>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
 
 export default () => { 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         const {target: {name, value}} = event;
         if(name === 'email'){
@@ -11,15 +13,29 @@ export default () => {
             setPassword(value)
         }
     }
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
         event.preventDefault();
+        try{
+            let data
+            if(newAccount){
+                //create account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data)
+        } catch(error){
+            console.log(error);
+        }
+        
     }
     return(
     <div>
         <form onSubmit={onSubmit}>
             <input type="text" placeholder="Email" required value={email} name="email" onChange={onChange}/>
             <input type="password" placeholder="Password" required value={password} name="password" onChange={onChange}/>
-            <input type="submit" value="Log In"/>
+            <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
         </form>
         <div>
             <button>Continue with Google</button>
